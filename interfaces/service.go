3d52557d1d549a1bcb60c98f793793f55ca813c9
Jiraphat (Saifah) <jiraphat.t@touchtechnologies.co.th>
2021-07-02 22:30:38 +0700

    Fix

--- interfaces/service.go
+++ interfaces/service.go
@@ -1,1 +1,10 @@
 package interfaces
+
+import "go-jwt/forms"
+
+type ServiceAdmin interface {
+	Create(data *string) (err error)
+	Read() (admin []*forms.Admin)
+	Update(data string) (result string)
+	Delete(data string) (err error)
+}
